2264224ef689fa386c6a92959bfe5cd0f13273af
Marcel Teruo Nakamine <marcel.nakamine@outlook.com>
2018-03-08 00:58:14 -0300

    add firebase configuration

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router/router'
 import store from './store'
 import Vuetify from 'vuetify'
+import * as firebase from 'firebase'
 import './registerServiceWorker'
 import 'vuetify/dist/vuetify.min.css'
 
@@ -13,5 +14,20 @@ Vue.use(Vuetify)
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAzS45SGsyBLAlUssu9GJoLUB-mj97OfUo',
+      authDomain: 'remember-the-bills.firebaseapp.com',
+      databaseURL: 'https://remember-the-bills.firebaseio.com',
+      projectId: 'remember-the-bills',
+      storageBucket: 'remember-the-bills.appspot.com',
+      messagingSenderId: '1018126444290'
+    })
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.$store.dispatch('autoSignIn', user)
+      }
+    })
+  }
 }).$mount('#app')
