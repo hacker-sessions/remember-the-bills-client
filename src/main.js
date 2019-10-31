07cc42ec9863cf6c48f94a794c92a4df2a408761
Marcel Nakamine <marcel.nakamine@outlook.com>
2019-10-31 03:44:24 -0300

    Upgrading Vuetify
    using vuetify by vue cli installation

--- src/main.js
+++ src/main.js
@@ -2,19 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router'
 import store from './store'
-import Vuetify from 'vuetify'
 import * as firebase from 'firebase'
 import Alert from './components/shared/Alert.vue'
 import './registerServiceWorker'
 import 'vuetify/dist/vuetify.min.css'
 import './style/loading.anim.css'
 import axios from 'axios'
+import vuetify from './plugins/vuetify';
 
 axios.defaults.headers.common['Accept'] = 'application/json'
 
 Vue.config.productionTip = false
-
-Vue.use(Vuetify)
 
 Vue.component('app-alert', Alert)
 
@@ -22,14 +20,17 @@ new Vue({
   router,
   store,
   render: h => h(App),
+  vuetify,
+
   created () {
     firebase.initializeApp({
-      apiKey: 'AIzaSyAzS45SGsyBLAlUssu9GJoLUB-mj97OfUo',
-      authDomain: 'remember-the-bills.firebaseapp.com',
-      databaseURL: 'https://remember-the-bills.firebaseio.com',
-      projectId: 'remember-the-bills',
-      storageBucket: 'remember-the-bills.appspot.com',
-      messagingSenderId: '1018126444290'
+      apiKey: "AIzaSyC0SBugGCbJ6zTvHWH9Uogb0XeKbo31jgU",
+      authDomain: "remember-the-bills-35eef.firebaseapp.com",
+      databaseURL: "https://remember-the-bills-35eef.firebaseio.com",
+      projectId: "remember-the-bills-35eef",
+      storageBucket: "remember-the-bills-35eef.appspot.com",
+      messagingSenderId: "980883720163",
+      appId: "1:980883720163:web:43dbc865b18099ebb482d6"
     })
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
